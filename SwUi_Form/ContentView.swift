0538cbb4c7d943dbaefdbb203191c6cf0c2b7f88
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-05-16 12:18:51 -0300

    initial commit

--- SwUi_Form/ContentView.swift
+++ SwUi_Form/ContentView.swift
@@ -7,10 +7,48 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    @State var isAutomatic = false
+    @State var brightValue = 50.00
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        Form{
+            
+            Section(header: Text("APPEARENCE")){
+                HStack {
+                    Text("Automatic")
+                    Spacer()
+                    Toggle("", isOn: $isAutomatic)
+                }
+                HStack {
+                    Text("Options")
+                    Spacer()
+                    Text("Light Until Sunset")
+                }
+            }
+
+            Section(header: Text("BRIGHTNESS")) {
+                HStack {
+                    Image(systemName: "sun.max.fill")
+                    
+                    Slider(value: $brightValue, in: 0...100)
+                        .onChange(of: brightValue) { newValue in
+                            print("📍 brightValue: \(brightValue)")
+                        }
+                    
+                    Image(systemName: "sun.max.fill")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                     
+                }
+            }
+            
+            
+        }
     }
 }
 
