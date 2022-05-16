cd55c21d9610a1f2bba0064b9b8ca3dc4a3676e1
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-05-16 11:24:37 -0300

    Initial Commit

---
+++ SwUi_Form/SwUi_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwUi_FormApp.swift
+//  SwUi_Form
+//
+//  Created by Marcelo Sampaio on 16/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwUi_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
